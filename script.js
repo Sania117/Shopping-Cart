1c64328f8bd67cb0f71aea392f36c780b5e249f9
Sania117 <43829099+Sania117@users.noreply.github.com>
2019-04-02 15:43:08 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,33 @@
+var products = [];
+var prices = [];
+
+
+$(".add").click(function() {
+    // first, get product name and price
+    var name = $(".Name").val();
+    products.push(name);
+    var price = $(".Price").val();
+    prices.push(price);
+    
+    // next, create new element with name and price
+    var text = "<div class='item'>";
+    text = text + "<div>"+name+"</div>";
+    text = text + "<div>"+price+"</div>";
+    text = text + "</div>" ;
+    
+    // next, append new element to shopping cart
+    $(".Shop").append(text);
+    
+    // get total number of products and display it
+    var items = products.length;
+    $(".totalNumber").text(items);
+});
+$(".finalize").click(function() {
+    var finalPrice = 0;
+    prices.forEach(function(price) {
+        var priceAsNumber;
+        priceAsNumber = Number(price);
+        finalPrice = finalPrice + priceAsNumber;
+    });
+   $(".result").text(" Thank you for your purchase your total is $" + finalPrice + ".");
+});
